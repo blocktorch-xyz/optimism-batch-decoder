65f6156361d919d0f7d13376acb49e7410299e75
iuwqyir <toomas.oosalu@gmail.com>
2024-01-16 15:37:27 +0200

    export test function with example data and fetch from provider

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import fs from 'fs'
 import path from 'path'
-import { extractBatcherTransaction } from './transactions/batcherTransaction'
+import { BatcherTransaction, extractBatcherTransaction } from './transactions/batcherTransaction'
+import { ethers } from 'ethers'
 
-
-(async () => {
+export const testWithExampleData = async (): Promise<BatcherTransaction> => {
   const examplePath = path.join(path.dirname(__dirname), 'example-data/calldata.txt')
   const exampleCallData = fs.readFileSync(examplePath).toString()
   return await extractBatcherTransaction(exampleCallData)
-})()
+}
+
+export const decodeBatcherTransaction = async (txHash: string, providerUrl: string): Promise<BatcherTransaction> => {
+  const provider = new ethers.providers.JsonRpcProvider(providerUrl)
+  const tx = await provider.getTransaction(txHash)
+  if (!tx.data) throw new Error('Transaction is missing calldata')
+  return await extractBatcherTransaction(tx.data)
+}
