7bc010c28b18dea9aeb3c9196b418e7e510ccb59
iuwqyir <toomas.oosalu@gmail.com>
2024-01-16 13:16:48 +0200

    add example data

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ import path from 'path'
 
 
 (async () => {
+  const examplePath = path.join(path.dirname(__dirname), 'example-data/calldata.txt')
+  const exampleCallData = fs.readFileSync(examplePath).toString()
 })()
